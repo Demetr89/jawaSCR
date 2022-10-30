766a89664e43dfb35f531b69fa5e5616e1f83c38
Dikusar Dmyuro <dima.dikusar.89@gmail.com>
2022-10-30 20:16:54 +0200

    yrtj

---
+++ script.js
@@ -0,0 +1,16 @@
+const pricePerDroid = 800;
+console.log(pricePerDroid);
+
+const orderedQuantity = 6;
+console.log(orderedQuantity);
+
+const deliveryFee = 50;
+console.log(deliveryFee);
+
+const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+console.log(totalPrice);
+
+const message =
+  "You ordered droids worth ${totalPrice} credits. Delivery ${deliveryFee}  credits is included in total price.";
+
+console.log(message);
